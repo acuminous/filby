91a0f6147add3a2847b1cf340f8925bc8a22230c
Stephen Cresswell <229672+cressie176@users.noreply.github.com>
2024-01-07 12:56:32 +0000

    Lint - tabs

--- examples/javascript/routes/changelog-v1.js
+++ examples/javascript/routes/changelog-v1.js
@@ -1,47 +1,47 @@
 const createError = require('http-errors');
 
 const getChangelogSchema = {
-	querystring: {
-		type: "object",
-	  required: ["projection", "version"],
-		properties: {
-			projection: {
-				type: "string"
-			},
-			version: {
-				type: "integer"
-			}
-		}
-	}
+  querystring: {
+    type: "object",
+    required: ["projection", "version"],
+    properties: {
+      projection: {
+        type: "string"
+      },
+      version: {
+        type: "integer"
+      }
+    }
+  }
 };
 
 module.exports = (fastify, { rdf }, done) => {
 
-	fastify.get('/', { schema: getChangelogSchema }, async (request, reply) => {
+  fastify.get('/', { schema: getChangelogSchema }, async (request, reply) => {
 
-		const projection = await getProjection(request);
+    const projection = await getProjection(request);
 
-		const changeLog = await rdf.getChangeLog(projection);
-		if (changeLog.length === 0) throw createError(404, `Projection ${projection.name}-v${projection.version} has no change sets`);
+    const changeLog = await rdf.getChangeLog(projection);
+    if (changeLog.length === 0) throw createError(404, `Projection ${projection.name}-v${projection.version} has no change sets`);
 
-		const changeSet = changeLog[changeLog.length - 1];
+    const changeSet = changeLog[changeLog.length - 1];
 
-		reply.headers({
-			'Last-Modified': changeSet.lastModified.toUTCString(),
-			'ETag': changeSet.entityTag,
-			'Cache-Control': 'max-age=600, stale-while-revalidate=600, stale-if-error=86400',
-		});
+    reply.headers({
+      'Last-Modified': changeSet.lastModified.toUTCString(),
+      'ETag': changeSet.entityTag,
+      'Cache-Control': 'max-age=600, stale-while-revalidate=600, stale-if-error=86400',
+    });
 
-		return changeLog;
-	});
+    return changeLog;
+  });
 
-	async function getProjection(request) {
-		const name = String(request.query.projection);
-		const version = Number(request.query.version);
-		const projection = await rdf.getProjection(name, version);
-		if (!projection) throw createError(404, `Projection not found: ${name}-v${version}`);
-		return projection;
-	}
+  async function getProjection(request) {
+    const name = String(request.query.projection);
+    const version = Number(request.query.version);
+    const projection = await rdf.getProjection(name, version);
+    if (!projection) throw createError(404, `Projection not found: ${name}-v${version}`);
+    return projection;
+  }
 
-	done();
+  done();
 };
